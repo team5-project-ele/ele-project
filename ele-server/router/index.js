1ee0e547c1e705dcf948de9dd8286428d8255d24
Liuzhimin <276078149@qq.com>
2020-09-14 15:28:50 +0800

    1

---
+++ ele-server/router/index.js
@@ -0,0 +1,7 @@
+const KoaRouter = require('koa-router')
+const router = new KoaRouter()
+router.get('/test', (ctx, next) => {
+  ctx.body = 1
+})
+
+module.exports = router
---
+++ ele-server/server.js
@@ -0,0 +1,11 @@
+const Koa = require('koa')
+const router = require('./router')
+const app = new Koa()
+// 声明使用路由器
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+app.listen('5000', () => {
+  console.log('服务器启动成功')
+  console.log('服务器地址：http://localhost:5000')
+})
