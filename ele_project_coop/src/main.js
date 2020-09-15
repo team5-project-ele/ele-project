510c4c527e1eb1396e1525267c4d3e3f5f0fa810
Zhang Tao <191094610@qq.com>
2020-09-15 10:11:15 +0800

    03

--- ele_project_coop/src/main.js
+++ ele_project_coop/src/main.js
@@ -2,15 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'lib-flexible/flexible'
-<<<<<<< HEAD
-import VueRouter from 'vue-router';
-
-
-
-=======
 import { Icon } from 'vant'
 Vue.use(Icon)
->>>>>>> 65f92c09acac612268e391fdc2e82f7cbd26db91
 Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
