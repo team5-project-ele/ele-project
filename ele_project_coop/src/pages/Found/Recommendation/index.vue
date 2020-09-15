631c64a354d1e8512c1350a6d8ae91071aa304b2
xiaorui <1925406086@qq.com>
2020-09-15 14:11:08 +0800

    来自小蕊的金币页面

---
+++ ele_project_coop/src/pages/Found/Recommendation/index.vue
@@ -0,0 +1,36 @@
+// 推荐有奖页面
+<template>
+  <div class="recommendationContainer">
+    <!-- 大图 -->
+    <div class="recommendationImg">
+      <img src="./image/yaoqing.png" alt="">
+    </div>
+    <!-- 重要通知 -->
+    <div class="notice">
+      <!-- <van-icon name="volume" /> -->
+      <span>
+
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    name:'Recommendation'
+  }
+</script>
+<style lang="stylus">
+*
+  margin 0
+  padding 0
+  .recommendationContainer
+    height 1370px
+    width 100%
+    background rgb(249, 53, 93)
+    .recommendationImg
+      width 100%
+      height 850px
+      img 
+        width 100%
+        height 100%
+</style>
